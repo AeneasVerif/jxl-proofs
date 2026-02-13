4019f5f8bc8922ba5e3b2ba97bdd8422c911c8f1
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2026-02-13 10:49:36 -0800

    (With Son) fill out more proofs

--- lean/JxlProofs/Proofs.lean
+++ lean/JxlProofs/Proofs.lean
@@ -37,6 +37,7 @@ lemma ad_hoc (x: U32): x.val &&& 0xfff = x.val % 2^12 :=
 
 -- THEOREMZ
 
+@[progress]
 theorem bucket_index_is_in_bounds (hist: AnsHistogram) (inv: hist.invariant) (state: U32):
     bucket_index hist state ⦃ idx => idx < hist.buckets.len ⦄
 :=
@@ -62,6 +63,30 @@ theorem bucket_index_is_in_bounds (hist: AnsHistogram) (inv: hist.invariant) (st
             apply Nat.pow_div <;> scalar_tac
     assumption
 
+theorem bucket_index_eq {a} (self: AnsHistogram) (i: U32) (f: Usize -> U32 -> Result a):
+    (do
+      let i1 ← toResult (i &&& 4095#u32)
+      let i2 ← i1 >>> self.log_bucket_size
+      let i3 ← toResult (UScalar.cast UScalarTy.Usize i2)
+      f i3 i1) =
+    (do
+      let i3 ← bucket_index self i
+      f i3 (i &&& 4095#u32))
+  :=
+  by
+    simp [bucket_index,toResult]
+    intros v
+    intros h
+    congr
+    scalar_tac
+
+/- attribute [bv_tac_simps,simp] LOG_SUM_PROBS -/
+
+@[simp,scalar_tac x.val * y.val]
+theorem times_zero_or_1 (x y: U32) (h: y.val = 0 ∨ y.val = 1): x.val * y.val <= U32.max :=
+  by
+    cases h <;> scalar_tac
+
 set_option maxHeartbeats 1000000
 theorem read_does_not_panic (self : entropy_coding.ans.AnsHistogram) (inv: self.invariant) (br : bit_reader.BitReader) (state : Std.U32) :
     self.read br state ⦃ r => True ⦄
@@ -69,12 +94,14 @@ theorem read_does_not_panic (self : entropy_coding.ans.AnsHistogram) (inv: self.
   by
     unfold entropy_coding.ans.AnsHistogram.read
     simp at inv
-    split_conjs at inv
+    rcases inv with ⟨ inv0, inv1, inv2 ⟩
     simp_all only [global_simps]
+    rw [bucket_index_eq]
     progress*
     <;> try 
       have : map_to_alias.val = 0 ∨ map_to_alias.val = 1 := by scalar_tac
       cases this <;> scalar_tac
+    . simp_all [global_simps]
     . have : self.buckets.val.length.isPowerOfTwo := ⟨ _, by assumption ⟩
       scalar_tac
     . have : self.buckets.len = self.buckets.deref.length := rfl
@@ -86,18 +113,18 @@ theorem read_does_not_panic (self : entropy_coding.ans.AnsHistogram) (inv: self.
         have : pos.val < 4096 := by bv_tac 32
         scalar_tac
     . have : i10.val < 2^20 := by bv_tac 32
-      have : dist.val < 2^16 := by bv_tac 32
-      have : bucket = self.buckets.val[i1.val] := by
-        conv => lhs; simp[*,alloc.vec.Vec.deref]
+      have h : bucket = self.buckets.val[i3.val] := by
+        simp_all[alloc.vec.Vec.deref]
         sorry
-      have : bucket.invariant := by sorry
-      sorry -- need to get the invariant
+      have : bucket.invariant := by
+        have := inv2 bucket
+        simp [h] at this
+        simp [global_simps,h,this]
+      simp at this
+      split_conjs at this
+      have : dist1.val < 2^12 := by
+        simp [global_simps] at *
+        bv_tac 32
+      scalar_tac
     . sorry
     . sorry -- need to specify br.peek
-    . have : i1 = bucket_index self state := by
-        simp_all
-        simp [bucket_index]
-        sorry
-      -- apply bucket_index_is_in_bounds
-      -- contradiction
-      sorry
